9588105d906fe9c01bf18dbb2389bcb1f7538c9c
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2025-09-22 17:10:57 -0300

    Lista sem 4

---
+++ Listas - unidade1/Semana4/output/quest3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int i = 1, n1, n2, div1 = 0, div2 = 0;
+    printf("Digite os 2 numeros: ");
+    scanf("%d %d", &n1, &n2);
+
+    if(n1 != n2){
+        if(n1 > n2){
+            do{
+                if(n1 % i == 0){
+                    div1++;
+                }
+                if(n2 % i == 0){
+                    div2++;
+                }
+                i++;
+            }while(i < n1);
+        }else if(n1 < n2){
+            do{
+                if(n1 % i == 0){
+                    div1++;
+                }
+                if(n2 % i == 0){
+                    div2++;
+                }
+                i++;
+            }while(i < n2);
+        }
+    }
+    
+    if(div1 == n2 && div2 == n1){
+        printf("%d e %d sao numeros amigos", n1, n2);
+    }
+
+    return 0;
+}
