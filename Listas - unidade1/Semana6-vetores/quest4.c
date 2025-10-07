fd9d9f4c1ab86b51bb163b343586993484be787d
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2025-10-07 09:47:19 -0300

    att

--- Listas - unidade1/Semana6-vetores/quest4.c
+++ Listas - unidade1/Semana6-vetores/quest4.c
@@ -11,21 +11,32 @@ void mapa_tesouro(int tesouro[]){
 }
 
 int visita_segunda(int tesouro[]){
+    //Todos os 10 valores do vetor para verificar as ilhas visitadas são todos 0 inicialmente
+    //Um vetor boleano
     int visitado[10] = {0};
+
+    //Pela questão, a ilha inicial é sempre a ilha 0 e a proxima ilha é baseada na ilha atual
     int ilha_atual = 0, proxima_ilha;
 
+    /*Como a ilha 0 é o ponto de partida(primeira ilha), ela é considerada visitada,
+    portanto, a posição zero do vetor leva o valor 1*/
     visitado[0] = 1;
 
+    /*Laço de repetição que será executado para verificar qual ilha é visitado uma segunda vez
+    primeiro*/
     while(1){
+        /*proxima_ilha = tesouro[0], se proxima_ilha for 5, por exemplo,
+        a proxima ilha sera: tesouro[5] e assim por diante */
         proxima_ilha = tesouro[ilha_atual];
-
+        /*Se a proxima ilha já tiver sido visitado, é retornado o valor da variavel e o laço
+        vai parar*/
         if(visitado[proxima_ilha] == 1){
             return proxima_ilha;
         }
 
+        //atualização constante dos valores no laço
         visitado[proxima_ilha] = 1;
         ilha_atual = proxima_ilha;
-
     }
 
 }
--- Listas - unidade1/semana4-repeticao-b/quest7.c
+++ Listas - unidade1/semana4-repeticao-b/quest7.c
@@ -12,10 +12,23 @@ int main(){
         for(i = b; i<=a; i++){
             primo = 1;
 
+            /*Os primos são, para essa questão, todos os numeros acima de 1
+            por isso, se i for <= 1, ele não eh primo*/
             if(i <= 1){
                 primo = 0;
             }else{
-                for(j = 2; j*j<=i; j++){
+                /*Definição matematica: para determinar se um número é primo,
+                basta testar a divisibilidade por todos os números primos 
+                menores ou iguais à sua raiz quadrada: j²<=i, j<=√i. Exemplo:
+                se i = 36, j ira iterar de 2 até 6, casa por casa.
+                Caso quociente de i e j tenha resto zero, o segundo for para é
+                dado inicio a uma nova iteração em i*/
+
+                /*a = 36 e b = 30, i = 30: iteração j = 2, 3, 4, 5.
+                (limite maximo do for j é o inteiro da raiz de i). 
+                30 será divido por todos esses 4 numeros,
+                se ele for divisivel por plmns 1 deles, significa que que 30 não eh primo*/
+                for(j = 2; j<=sqrt(i); j++){
                     if(i%j == 0){
                         primo = 0;
                         break;
@@ -27,9 +40,6 @@ int main(){
             }
         }
     }
-
-
-
 
     return 0;
 }
