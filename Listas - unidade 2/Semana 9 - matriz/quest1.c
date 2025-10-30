dff21286c6649f1faaa7761723ae234c765f9f88
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2025-10-30 20:50:26 -0300

    lista2

---
+++ Listas - unidade 2/Semana 9 - matriz/quest1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ler_matriz(int agro[][100], int m, int n){
+    int i, j;
+
+    for(i = 0; i<m; i++){
+        for(j = 0; j<n; j++){
+            scanf("%d", &agro[i][j]);
+        }
+    }
+}
+
+void fertil(int agro[][100], int m, int n){
+    int i, j, cont = 0, cont2 = 0;
+    int irrigados[10][10] = {0};
+
+    for(i = 0; i<m; i++){
+        for(j = 0; j<n; j++){
+            if(agro[i][j] == 2){
+                if(i > 0 && i < m-1 && j > 0 && j <n-1){
+                    irrigados[i-1][j] = 1;
+                    irrigados[i+1][j] = 1;
+                    irrigados[i][j-1] = 1;
+                    irrigados[i][j+1] = 1;
+                }
+            }
+        }
+    }
+    for(i = 0; i <m; i++){
+        for(j = 0; j < n; j++){
+            if(agro[i][j] == 1){
+                if(irrigados[i][j] == 1){
+                    cont++;
+                }else{
+                    cont2++;
+                }
+            }
+        }
+    }
+    printf("%d %d", cont, cont2);
+
+}
+
+int main(){
+    int m, n;
+    printf("Digite o tamanho das matrizes(MXN):\n");
+    scanf("%d %d", &m, &n);
+
+    int agro[m][n];
+
+    if(m > 0 && m <=10){
+        if(n > 0 && n<=10){
+            ler_matriz(agro, m, n);
+            fertil(agro, m, n);
+        }
+    }
+
+    return 0;
+}
