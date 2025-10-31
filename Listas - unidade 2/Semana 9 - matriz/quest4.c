e741c53d413148001d3eb583642c2c7a6ac5ada2
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2025-10-31 11:51:43 -0300

    lista2

--- Listas - unidade 2/Semana 9 - matriz/quest4.c
+++ Listas - unidade 2/Semana 9 - matriz/quest4.c
@@ -13,7 +13,39 @@ void ler_matriz(int matriz[][100], int m){
     }
 }
 
+void viagem(int matriz[][100], int m, int x, int z){
+    int y;
+
+    int custo_direto = matriz[x][z];
+    int melhor_custo = custo_direto;
+    int melhory = -1;
+
+    for(y = 0; y<m; y++){
+        if(y != x && y != z){
+            int custo = matriz[x][y] + matriz[y][z];
+            if(custo < melhor_custo){
+                melhor_custo = custo;
+                melhory = y;
+            }
+        }
+    }
+
+    if(melhor_custo == matriz[x][z]){
+        printf("%d-%d", x, z);
+    }else if(melhory != -1){
+        printf("%d-%d-%d", x, y, z);
+    }
+
+    
+}
+
 
 int main(){
-    
+    int m, x, z;
+
+    scanf("%d", &m);
+    int viagem[m][m];
+    scanf("%d %d", &x, &z);
+
+    ler_matriz(viagem, m);
 }
