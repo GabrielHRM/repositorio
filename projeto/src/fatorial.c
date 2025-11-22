08f9480f5cedecaf9c15d60f20e2210bdceaecf8
GabrielHRM <henrique.melo.110@ufrn.edu.br>
2025-11-22 16:51:49 -0300

    projeto

--- projeto/src/fatorial.c
+++ projeto/src/fatorial.c
@@ -21,44 +21,61 @@ long long int fatinicial(long long int x){
 }
 
 char *fatoracao_primos(long long int n){
+    /*3 indices: 1 para os fatores primos, outro para os indices dos vetores
+    das potencias dos fatores primos e das potencias e o ultimo é para
+    percorrer os 2 vetores e transofrma-los em strings*/
     int i = 2;
     int j = 0;
     int k;
     int potencia;
+    /*Alocação de memoria dos vetores dos fatores primos e suas potencia.*/
     int *primos = (int*)malloc(2000*sizeof(int));
+    int *potencia_primos = (int*)malloc(2000*sizeof(int));
+    /*Alocação de memoria das strings que serão manipuladas para levar
+    o resultado até o histórico*/
     char *entrada = (char*)malloc(2000*sizeof(char));
     char *string_final = (char*)malloc(2000*sizeof(char));
-    int *potencia_primos = (int*)malloc(2000*sizeof(int));
-    
+
     
     entrada[0] = '\0';
     string_final[0] = '\0';
 
     sprintf(string_final, "%lld = ", n);
+    /*O esquema é ir reduzindo o valor de n até 1 por divisões.
+    Enquanto o resto de divisão de n pelo i inicial(i = 2) for zero
+    n será dividido por i até que o resto seja != 0*/
     while(n>1){
         potencia = 0;
         while(n%i == 0){
             potencia++;
             n = n/i;
         }
+        /*Caso a potencia seja maior que zero, é guardado os respectivos valores
+        nos vetores previamente alocados*/
         if(potencia > 0){
             primos[j] = i;
             potencia_primos[j] = potencia;
             j++;
         }
+        //Por fim, o indice i é atualizado
         i++;
     }
 
     long int tamanho = j;
 
     for(k = 0; k<tamanho; k++){
+        //Preenchendo a string entrada com o resultado da fatoração
         sprintf(entrada, "%d^%d", primos[k], potencia_primos[k]);
+        //Concatena a string entrada para string final que será retornada
         strcat(string_final, entrada);
+        //Medida de segurança para menos o ultimo indice da string tenha o ' x '
         if(k < tamanho - 1){
             strcat(string_final, " x ");
         }
     }
 
+    //libera os espaços de memoria das strings usadas, menos a string_final.
+    //Essa vai ser liberado posteriormente.
     free(primos);
     free(potencia_primos);
     free(entrada);
